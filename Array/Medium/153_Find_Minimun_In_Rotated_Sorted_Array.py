bdf1532c3317b60f6b311840ce332db49c71f32a
yutingwu <ding25025@gmail.com>
2023-10-25 14:21:54 +0800

    feat(153_Find_Minimun_In_Rotated_Sorted_Array.py): LeetCode_153

---
+++ Array/Medium/153_Find_Minimun_In_Rotated_Sorted_Array.py
@@ -0,0 +1,21 @@
+"""
+ Suppose an array of length n sorted in ascending order is rotated between 1 and n times.
+ Time - O(logN)
+ Space - O(1)
+"""
+
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[right]:
+                # move to left side
+                right = mid
+            else:
+                # move right side
+                left = mid + 1
+        return nums[right]
