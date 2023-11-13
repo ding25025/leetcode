28773a12c7577da12fedd126ec88419314bd503c
yutingwu <ding25025@gmail.com>
2023-11-13 14:44:45 +0800

    feat(300_Longest_Increasing_Subsequence.py): LeetCode_300

---
+++ DynamicProgramming/Medium/300_Longest_Increasing_Subsequence.py
@@ -0,0 +1,31 @@
+"""
+Given an integer array nums, return the length of the longest strictly increasing 
+subsequence.
+Time O(nlogn)
+Space O(N)
+
+"""
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        sub = [nums[0]]
+
+        for num in nums:
+            if num > sub[-1]:
+                sub.append(num)
+            else:
+                # Use binary search to find the correct position to replace
+                left, right = 0, len(sub) - 1
+                while left < right:
+                    mid = (left + right) // 2
+                    if sub[mid] < num:
+                        left = mid + 1
+                    else:
+                        right = mid
+                sub[left] = num
+
+        return len(sub)
